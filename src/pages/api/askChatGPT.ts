446e2c512fec6dcadbcef76ef2677ebcfd1d8b12
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2023-01-29 12:43:37 +0100

    chat gpt client

---
+++ src/pages/api/askChatGPT.ts
@@ -0,0 +1,78 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+const MODEL = {
+    best: "text-davinci-003",
+    strong: "text-curie-001",
+    medium: "text-babbage-001",
+    weak: "text-ada-001"
+}
+
+type Data = {
+    response: string
+}
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    // Check if Text is provided
+    if (!req.body.text) {
+        res.status(400).json({
+            response: "Please input some text!"
+        });
+        return;
+    }
+
+    // Check if input is not against openai's policies
+    const prohibited = await flagged(req.body.text);
+
+    if (prohibited) {
+        res.status(200).json({
+            response: "Your input seems to violate OpenAI's policies."
+        });
+        return;
+    }
+
+    const response = await askChatGPT(req.body.text);
+
+    res.status(200).json({
+        response: response
+    });
+}
+
+async function askChatGPT(prompt: string) {
+    const {Configuration, OpenAIApi} = require("openai");
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+        model: MODEL.best,
+        prompt: prompt,
+        temperature: 0,
+        max_tokens: 100,
+    });
+
+    if (response.status === 200)
+        return response.data.choices[0].text;
+    else
+        return "Error fetching ...";
+}
+
+async function flagged(input: string) {
+    const response = await fetch("http://localhost:3000/api/moderation", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            text: input
+        })
+    });
+
+    const moderation = await response.json();
+
+    return moderation.flagged;
+}
---
+++ src/pages/api/moderation.ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next'
+import axios from "axios";
+
+type Data = {
+    flagged: boolean
+}
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+
+    if (!req.body.text){
+        res.status(400).json({
+            flagged: false
+        });
+        return;
+    }
+
+    let violation = false;
+
+    const data = await axios("https://api.openai.com/v1/moderations",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + process.env.OPENAI_API_KEY
+        },
+        data: {
+            input: req.body.text
+        }
+    });
+
+    if (data.status === 200){
+        violation = data.data.results[0].flagged;
+    }
+
+    res.status(200).json({
+        flagged: violation
+    })
+}
