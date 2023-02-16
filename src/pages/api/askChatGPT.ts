1ff00c55fa9f542b319023b822e5562fe5a20c61
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2023-02-16 04:47:21 +0100

    REEEEEEEEEEE

--- src/pages/api/askChatGPT.ts
+++ src/pages/api/askChatGPT.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
-import axios from "axios";
 
 const MODEL = {
     best: "text-davinci-003",
